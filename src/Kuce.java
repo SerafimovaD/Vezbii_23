8cedac8123120154c40b8287917dfba990406310
SerafimovaD <daci.serafimova@gmail.com>
2024-01-11 11:29:04 +0100

    Add first java program

---
+++ src/Kuce.java
@@ -0,0 +1,7 @@
+
+public class Kuce { // SUPERCLASS Kuce
+		public void osobina(){ // metod osobina
+		System.out.println("Kuceto lae");
+		}
+	}
+
---
+++ src/LoveckoKuce.java
@@ -0,0 +1,6 @@
+
+public class LoveckoKuce extends Kuce{ // SUBCLASS Pas
+		public void osobina(){ // metod osobina
+		System.out.println("Loveckoto kuce duska");
+		}
+}
